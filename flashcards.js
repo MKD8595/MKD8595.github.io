78c2eb14dd0fc6231bf286f286d978b00777c890
MKD8595 <58298145+MKD8595@users.noreply.github.com>
2022-04-14 23:56:04 +1000

    JSON test

---
+++ flashcards.js
@@ -0,0 +1,19 @@
+function generateQuestion() {
+
+    //read data
+    data = fetch("data.json")
+    .then(response => response.json())
+    .then(json => console.log(json));
+    let questionData = JSON.parse(data)
+    //fill question
+    
+    //hide answer
+
+}
+
+function showAnwer() {
+
+    //fill question
+
+
+}
